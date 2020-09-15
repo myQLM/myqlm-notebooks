3a486e483ff8bd75eb0490b1c52861f4a87bf872
Arnaud Gazda <arnaud.gazda@atos.net>
2020-09-15 12:09:18 +0000

    First commit

---
+++ tutorials/interoperability/Pyquil.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Start a Pyquil Server 
+
+HOST=127.0.0.1
+PORT=15011
+
+
+#
+# pre
+#
+pre()
+{
+if [[ -x /usr/local/bin/qvm ]]; then
+    nohup qvm --server --host $HOST --port $PORT &>$outfile &
+    echo "$!" >$outfile.pid
+    while ((sec < 5)); do
+        sleep 1
+        netstat -an | grep -qE "$HOST:$PORT.*LISTEN" && break
+        ((sec++))
+    done
+fi
+}
+
+
+#
+# Post
+#
+post()
+{
+kill $(cat $outfile.pid) >/dev/null
+return
+}
+
+
+#
+# Main
+#
+snipet=$1
+outfile="$2"
+
+case $snipet in
+    pre)
+        pre
+        ;;
+    post)
+        post
+        ;;
+esac
+

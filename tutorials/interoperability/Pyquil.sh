12b1c227979c857d9dcd08abc179a3e19e6a251b
Jerome Pioux <jerome.pioux@atos.net>
2021-12-08 14:06:56 -0700

    1.5.1 release

--- tutorials/interoperability/Pyquil.sh
+++ tutorials/interoperability/Pyquil.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-# Start a Pyquil Server 
+# Start Pyquil Servers
 
-HOST=127.0.0.1
-PORT=15011
+declare -A server_path server_opts server_host server_port server_pidf
+
+servers="qvm quilc"
+
+server_path[qvm]="/usr/local/bin/qvm"
+server_opts[qvm]="--server"
+server_host[qvm]="127.0.0.1"
+server_port[qvm]="15011"
+server_pidf[qvm]="${outfile%%.*}_qvm.pid"
+
+server_path[quilc]="/usr/local/bin/quilc"
+server_opts[quilc]="--server-mode-rpc"
+server_host[quilc]="127.0.0.1"
+server_port[quilc]="15012"
+server_pidf[quilc]="${outfile%%.*}_quilc.pid"
 
 
 #
@@ -10,15 +23,19 @@ PORT=15011
 #
 pre()
 {
-if [[ -x /usr/local/bin/qvm ]]; then
-    nohup qvm --server --host $HOST --port $PORT &>$outfile &
-    echo "$!" >$outfile.pid
-    while ((sec < 5)); do
-        sleep 1
-        netstat -an | grep -qE "$HOST:$PORT.*LISTEN" && break
-        ((sec++))
-    done
-fi
+for server in $servers; do
+    if [[ -x ${server_path[$server]} ]]; then
+        cmd="nohup $server ${server_opts[$server]} --host ${server_host[$server]} --port ${server_port[$server]}"
+        echo -e "\n> $cmd" >>$outfile
+        eval $cmd &>>$outfile &
+        echo "$!" >${server_pidf[$server]}
+        while ((sec < 5)); do
+            sleep 1
+            netstat -an | grep -qE "${server_host[$server]}:${server_port[$server]}.*LISTEN" && break
+            ((sec++))
+        done
+    fi
+done
 }
 
 
@@ -27,7 +44,9 @@ fi
 #
 post()
 {
-kill $(cat $outfile.pid 2>/dev/null) &>/dev/null
+for server in $servers; do
+    kill $(cat ${server_pidf[$server]} 2>/dev/null) &>/dev/null
+done
 return
 }
 
